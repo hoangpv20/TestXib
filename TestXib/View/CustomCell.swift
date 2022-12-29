ab0904f11fde7953a6382bbcd3bd26c15c221e34
hoangpv20 <viethoang211200@gmail.com>
2022-12-29 09:17:28 +0700

    second edit (edit cell)

--- TestXib/View/CustomCell.swift
+++ TestXib/View/CustomCell.swift
@@ -40,19 +40,15 @@ class CustomCell: UITableViewCell {
     func textToImage(drawText text: String, inImage image: UIImage, atPoint point: CGPoint) -> UIImage {
         let textColor = UIColor.white
         let textFont = UIFont(name: "Sarabun-Regular", size: 14)!
-
         let scale = UIScreen.main.scale
         UIGraphicsBeginImageContextWithOptions(image.size, false, scale)
-
         let textFontAttributes = [
             NSAttributedString.Key.font: textFont,
             NSAttributedString.Key.foregroundColor: textColor,
             ] as [NSAttributedString.Key : Any]
         image.draw(in: CGRect(origin: CGPoint.zero, size: image.size))
-
         let rect = CGRect(origin: point, size: image.size)
         text.draw(in: rect, withAttributes: textFontAttributes)
-
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
 
